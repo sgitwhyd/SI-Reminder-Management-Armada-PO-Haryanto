2b0fc3d06683e58adb4d89ee42bd8c19ea7ce584
Banisaputra <bani.s26shafa@gmail.com>
2024-02-15 17:09:03 +0700

    db update

--- sql.sql
+++ sql.sql
@@ -16,23 +16,37 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`si_armada` /*!40100 DEFAULT CHARACTER S
 
 USE `si_armada`;
 
-/*Table structure for table `failed_jobs` */
+/*Table structure for table `master_armada` */
 
-DROP TABLE IF EXISTS `failed_jobs`;
+DROP TABLE IF EXISTS `master_armada`;
 
-CREATE TABLE `failed_jobs` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `master_armada` (
+  `id_armada` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `no_polisi` varchar(20) NOT NULL,
+  `no_lambung` varchar(100) NOT NULL,
+  `no_stnk` varchar(100) NOT NULL,
+  `tahun` int(5) unsigned NOT NULL,
+  `trayek` varchar(100) NOT NULL,
+  `jenis_trayek` enum('AKAP','AKDP','PARIWISATA','MPU') NOT NULL DEFAULT 'AKAP',
+  PRIMARY KEY (`id_armada`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-/*Data for the table `failed_jobs` */
+/*Data for the table `master_armada` */
+
+/*Table structure for table `master_sparepart` */
+
+DROP TABLE IF EXISTS `master_sparepart`;
+
+CREATE TABLE `master_sparepart` (
+  `id_sp` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `no_sp` varchar(100) NOT NULL,
+  `nama_sp` varchar(255) NOT NULL,
+  `stock_sp` int(11) unsigned NOT NULL,
+  `status` enum('READY','KOSONG') NOT NULL DEFAULT 'READY',
+  PRIMARY KEY (`id_sp`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `master_sparepart` */
 
 /*Table structure for table `migrations` */
 
@@ -53,41 +67,6 @@ insert  into `migrations`(`id`,`migration`,`batch`) values
 (3,'2019_08_19_000000_create_failed_jobs_table',1),
 (4,'2019_12_14_000001_create_personal_access_tokens_table',1);
 
-/*Table structure for table `password_reset_tokens` */
-
-DROP TABLE IF EXISTS `password_reset_tokens`;
-
-CREATE TABLE `password_reset_tokens` (
-  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-/*Data for the table `password_reset_tokens` */
-
-/*Table structure for table `personal_access_tokens` */
-
-DROP TABLE IF EXISTS `personal_access_tokens`;
-
-CREATE TABLE `personal_access_tokens` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `tokenable_type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tokenable_id` bigint(20) unsigned NOT NULL,
-  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `last_used_at` timestamp NULL DEFAULT NULL,
-  `expires_at` timestamp NULL DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
-  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-/*Data for the table `personal_access_tokens` */
-
 /*Table structure for table `users` */
 
 DROP TABLE IF EXISTS `users`;
@@ -96,9 +75,8 @@ CREATE TABLE `users` (
   `id_user` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('USER','ADMIN') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'USER',
   `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `verify_key` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `is_active` tinyint(1) unsigned NOT NULL DEFAULT 1,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
@@ -108,9 +86,9 @@ CREATE TABLE `users` (
 
 /*Data for the table `users` */
 
-insert  into `users`(`id_user`,`username`,`password`,`role`,`email`,`verify_key`,`is_active`,`created_at`,`updated_at`) values 
-(1,'johan','$2y$12$oYWuOCSjAB7GOGgSByolLufKKg6XAUxMyhXlceNUaj0DBGh/46vCa','user','johan@gmail.com','',1,'2024-02-13 10:02:22','2024-02-13 10:02:22'),
-(2,'anton','$2y$12$4UMtD3zg/qlXVn34LRls1OgNSoHzsDQg8DkcVKbn882UPUDPCBuHu','user','anton@gmail.com','',1,'2024-02-13 10:08:01','2024-02-13 10:08:01');
+insert  into `users`(`id_user`,`username`,`password`,`role`,`email`,`is_active`,`created_at`,`updated_at`) values 
+(1,'johan','$2y$12$oYWuOCSjAB7GOGgSByolLufKKg6XAUxMyhXlceNUaj0DBGh/46vCa','USER','johan@gmail.com',1,'2024-02-13 10:02:22','2024-02-13 10:02:22'),
+(2,'anton','$2y$12$4UMtD3zg/qlXVn34LRls1OgNSoHzsDQg8DkcVKbn882UPUDPCBuHu','USER','anton@gmail.com',1,'2024-02-13 10:08:01','2024-02-13 10:08:01');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
