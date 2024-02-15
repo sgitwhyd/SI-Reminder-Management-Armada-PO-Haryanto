d8d46d92fa2f6968b0a9952de9a3af5718c2387c
banisaputra <bani.s26shafa@gmail.com>
2024-02-15 23:36:03 +0700

    sparepart

---
+++ app/Models/M_sparepart.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class M_sparepart extends Model
+{
+    use HasFactory;
+
+    protected $table = 'master_sparepart';
+    
+}
