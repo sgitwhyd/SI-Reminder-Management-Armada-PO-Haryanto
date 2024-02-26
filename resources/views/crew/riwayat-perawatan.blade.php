0d962e82883ad7dcf1b4eedef7c49b17d63d5c5d
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-02-26 23:47:56 +0700

    style: format table using datatable & adding signature image view

--- resources/views/crew/riwayat-perawatan.blade.php
+++ resources/views/crew/riwayat-perawatan.blade.php
@@ -7,54 +7,60 @@
     <h1>
       Riwayat Perawatan
     </h1>
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <th>
-            Tanggal
-          </th>
-          <th>
-            Oli Gardan
-          </th>
-          <th>
-            Oli Mesin
-          </th>
-          <th>
-            Oli Transmisi
-          </th>
-          <th>
-            Status
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($data as $index => $item)
-        <tr>
-          <td>
-            {{ $item->tanggal }}
-          </td>
-          <td>
-            {{ $item->oli_gardan }}
-          </td>
-          <td>
-            {{ $item->oli_mesin }}
-          </td>
-          <td>
-            {{ $item->oli_transmisi }}
-          </td>
-          <td>
-            {{ $item->status }}
-          </td>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
-    {{ $data->links() }}
+    <div class="card shadow">
+      <div class="card-body">
+        <table class="table" id="dataPerawatan">
+          <thead>
+            <tr>
+              <th>
+                Tanggal
+              </th>
+              <th>
+                Oli Gardan
+              </th>
+              <th>
+                Oli Mesin
+              </th>
+              <th>
+                Oli Transmisi
+              </th>
+              <th>
+                Tanda Tangan Kepala Gudang
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($data as $index => $item)
+            <tr>
+              <td>
+                {{ $item->tanggal }}
+              </td>
+              <td>{{ number_format((float)$item->oli_gardan, 0, '.') }} KM</td>
+              <td>{{ number_format((float)$item->oli_mesin, 0, '.') }} KM</td>
+              <td>{{ number_format((float)$item->oli_transmisi, 0, '.') }} KM</td>
+              <td>
+                <img src="{{ asset('storage/' . $item->ttd_kepala_gudang) }}" alt="ttd_kepala_gudang" width="100">
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </div>
 @endsection
 
 @section('script')
 
+<script>
+$('#dataPerawatan').DataTable({
+  autoWidth: true,
+  "lengthMenu": [
+    [10, 25, 50, -1],
+    [10, 25, 50, "All"]
+  ]
+});
+</script>
 
 @endsection
