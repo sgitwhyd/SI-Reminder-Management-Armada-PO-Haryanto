c4205218a95078cee209deb60dc297b90035101a
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-02-26 23:48:21 +0700

    style: format table data perbaikan using datatable

--- resources/views/crew/riwayat-perbaikan.blade.php
+++ resources/views/crew/riwayat-perbaikan.blade.php
@@ -7,71 +7,77 @@
     <h1>
       Riwayat Perbaikan
     </h1>
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <th>
-            Tanggal
-          </th>
-          <th>
-            Nama Sparepart
-          </th>
-          <th>
-            Part No,
-          </th>
-          <th>
-            Jumlah
-          </th>
-          <th>
-            Nominal
-          </th>
-          <th>
-            Keterangan
-          </th>
-          <th>
-            Status
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($data as $index => $item)
-        <tr>
-          <td>
-            {{ $item->created_at }}
-          </td>
-          <td>
-            @foreach($item->spareparts as $sparepart)
-            {{ $sparepart->nama_sparepart }}, <br>
+    <div class="card shadow">
+      <div class="card-body">
+        <table class="table" id="dataPerbaikan">
+          <thead>
+            <tr>
+              <th>
+                Tanggal
+              </th>
+              <th>
+                Nama Sparepart
+              </th>
+              <th>
+                Part No,
+              </th>
+              <th>
+                Jumlah
+              </th>
+              <th>
+                Nominal
+              </th>
+              <th>
+                Keterangan
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($data as $index => $item)
+            <tr>
+              <td>
+                {{ $item->created_at }}
+              </td>
+              <td>
+                @foreach($item->spareparts as $sparepart)
+                {{ $sparepart->nama_sparepart }}, <br>
+                @endforeach
+              </td>
+              <td>
+                @foreach($item->spareparts as $sparepart)
+                {{ $sparepart->kode_sparepart }}, <br>
+                @endforeach
+              </td>
+              <td>
+                {{ $item->spareparts->count() }}
+              </td>
+              <td>
+                {{ $item->biaya }}
+              </td>
+              <td>
+                {{ $item->keterangan }}
+              </td>
+            </tr>
             @endforeach
-          </td>
-          <td>
-            @foreach($item->spareparts as $sparepart)
-            {{ $sparepart->kode_sparepart }}, <br>
-            @endforeach
-          </td>
-          <td>
-            {{ $item->spareparts->count() }}
-          </td>
-          <td>
-            {{ $item->biaya }}
-          </td>
-          <td>
-            {{ $item->keterangan }}
-          </td>
-          <td>
-            {{ $item->status }}
-          </td>
-        </tr>
-        @endforeach
 
-      </tbody>
-    </table>
-    {{ $data->links() }}
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </div>
 @endsection
 
 @section('script')
 
+<script>
+$('#dataPerbaikan').DataTable({
+  autoWidth: true,
+  "lengthMenu": [
+    [10, 25, 50, -1],
+    [10, 25, 50, "All"]
+  ]
+});
+</script>
 
 @endsection
