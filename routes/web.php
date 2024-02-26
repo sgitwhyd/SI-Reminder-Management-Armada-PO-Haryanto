d857ea4de41222cb8336d80c649048cd44a40240
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-02-26 23:50:37 +0700

    feat: adjust route and adding some route for perbaikan, perawatan, rammpcheck

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,12 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ArmadaController;
 use App\Http\Controllers\CrewController;
+use App\Http\Controllers\KepalaGudangController;
 use App\Http\Controllers\MekanikController;
 use App\Http\Controllers\SparepartController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PerawatanController;
+use App\Http\Controllers\PerbaikanController;
 use App\Http\Controllers\RampcheckController;
 
 /*
@@ -32,7 +34,7 @@ Route::post('/register', [AuthController::class, 'registerVerify']);
 Route::get('/forgot-pass', [AuthController::class, 'forgotPassword']);
 Route::get('/logout', [AuthController::class, 'logout']);
 
-Route::middleware(['auth', 'checkRole:KEPALA-GUDANG'])->prefix('kepala-gudang')->group(function () {
+Route::middleware(['auth', 'checkRole:KEPALA-GUDANG'])->prefix('kepala-gudang')->as('kepala_gudang.')->group(function () {
     Route::get('/dashboard', [HomeController::class, 'indexKepalaGudang']);
     // armada
     Route::get('/armada', [ArmadaController::class, 'index']);
@@ -41,37 +43,59 @@ Route::middleware(['auth', 'checkRole:KEPALA-GUDANG'])->prefix('kepala-gudang')-
     Route::put('/armada/update/', [ArmadaController::class, 'update']);
     Route::delete('armada/delete/{id}', [ArmadaController::class, 'destroy']);
 
-    // sparepart
-    Route::get('/sparepart', [SparepartController::class, 'index']);
-    Route::post('/sparepart', [SparepartController::class, 'store']);
-    Route::post('/sparepart/edit', [SparepartController::class, 'edit']);
-    Route::put('/sparepart/update', [SparepartController::class, 'update']);
-    Route::delete('sparepart/delete/{id}', [SparepartController::class, 'destroy']);
+    Route::group(['prefix' => 'sparepart'], function () {
+        // sparepart
+        Route::get('/', [SparepartController::class, 'index']);
+        Route::post('/', [SparepartController::class, 'store']);
+        Route::post('/edit', [SparepartController::class, 'edit']);
+        Route::put('/update', [SparepartController::class, 'update']);
+        Route::delete('sparepart/delete/{id}', [SparepartController::class, 'destroy']);
+        // riwayat sparepart
+        Route::get('/riwayat', [SparepartController::class, 'riwayatSparepart']);
+    });
+
 
     // user
-    Route::get('/user', [UserController::class, 'index']);
-    Route::post('/user', [UserController::class, 'store']);
-    Route::post('/user/edit', [UserController::class, 'edit']);
-    Route::put('/user/update', [UserController::class, 'update']);
-    Route::delete('user/delete/{id}', [UserController::class, 'destroy']);
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/', [UserController::class, 'index']);
+        Route::post('/', [UserController::class, 'store']);
+        Route::post('/edit', [UserController::class, 'edit']);
+        Route::put('/update', [UserController::class, 'update']);
+        Route::delete('user/delete/{id}', [UserController::class, 'destroy']);
+
+    });
+
+    // perbaikan
+    Route::group(['prefix' => 'perbaikan'], function () {
+        Route::get('/', [PerbaikanController::class, 'showPerbaikan']);
+        Route::get('/buat-perbaikan', [PerbaikanController::class, 'createPerbaikan']);
+        Route::post('/buat-perbaikan', [PerbaikanController::class, 'storePerbaikan'])->name('buat-perbaikan');
+        Route::get('/detail/{id}', [PerbaikanController::class, 'detailPerbaikan']);
+    });
 
     // perawatan
-    Route::get('/perawatan', [PerawatanController::class, 'index']);
-    Route::get('/perawatan/add', [PerawatanController::class, 'create']);
-    Route::post('/perawatan', [PerawatanController::class,'store']);
-    Route::post('/perawatan/edit', [PerawatanController::class, 'edit']);
-    Route::put('/perawatan/update', [PerawatanController::class, 'update']);
-    Route::delete('perawatan/delete/{id}', [PerawatanController::class, 'destroy']);
+    Route::group(['prefix' => 'perawatan'], function () {
+        Route::get('/', [PerawatanController::class, 'index']);
+        Route::get('/buat-perawatan', [PerawatanController::class, 'create']);
+        Route::post('/buat-perawatan', [PerawatanController::class,'store'])->name('buat-perawatan');
+        Route::post('/edit/{id}', [PerawatanController::class, 'edit']);
+        Route::put('/update/{id}', [PerawatanController::class, 'update']);
+        Route::delete('/delete/{id}', [PerawatanController::class, 'destroy']);
+
+    });
 
     // rampcheck
-    Route::get('/rampcheck', [RampcheckController::class, 'index']);
-    Route::get('/rampcheck/add', [RampcheckController::class, 'create']);
-    Route::get('/rampcheck/show/{id}', [RampcheckController::class,'show']);
-    Route::post('/rampcheck', [RampcheckController::class,'store']);
-    Route::get('/rampcheck/edit/{id}', [RampcheckController::class, 'edit']);
-    Route::put('/rampcheck/update', [RampcheckController::class, 'update']);
-    Route::delete('rampcheck/delete/{id}', [RampcheckController::class, 'destroy']);
-    Route::get('/rampcheck/pdf/{id}', [RampcheckController::class, 'rampcheckPDF']);
+    Route::group(['prefix' => 'rampcheck'], function () {
+        Route::get('/', [RampcheckController::class, 'index']);
+        Route::get('/add', [RampcheckController::class, 'create']);
+        Route::get('/show/{id}', [RampcheckController::class,'show']);
+        Route::post('/', [RampcheckController::class,'store']);
+        Route::get('/edit/{id}', [RampcheckController::class, 'edit']);
+        Route::put('/update', [RampcheckController::class, 'update']);
+        Route::delete('rampcheck/delete/{id}', [RampcheckController::class, 'destroy']);
+        Route::get('/pdf/{id}', [RampcheckController::class, 'rampcheckPDF']);
+
+    });
 
 });
 
@@ -79,14 +103,17 @@ Route::middleware(['auth', 'checkRole:CREW'])->prefix('crew')->group(function ()
     Route::get('/dashboard', [CrewController::class, 'index']);
     Route::get('/riwayat-perbaikan', [CrewController::class, 'riwayatPerbaikan']);
     Route::get('/riwayat-perawatan', [CrewController::class, 'riwayatPerawatan']);
-    Route::get('/riwayat-rampcheck', [CrewController::class, 'riwayatRampcheck']);
 });
 
 Route::middleware(['auth', 'checkRole:MEKANIK'])->prefix('mekanik')->as('mekanik.')->group(function () {
+    // perawatan
     Route::get('/perawatan', [MekanikController::class, 'showPerawatan']);
     Route::get('/tambah-perawatan', [MekanikController::class, 'createPerawatan']);
     Route::post('/tambah-perawatan', [MekanikController::class, 'storePerawatan'])->name('tambah-perawatan');
+    // perbaikan
     Route::get('/perbaikan', [MekanikController::class, 'showPerbaikan']);
     Route::get('/tambah-perbaikan', [MekanikController::class, 'createPerbaikan']);
     Route::post('/tambah-perbaikan', [MekanikController::class, 'storePerbaikan'])->name('tambah-perbaikan');
+
+    Route::get('/suku-cadang', [MekanikController::class, 'showSukuCadang']);
 });
