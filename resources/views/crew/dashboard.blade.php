ebea2116ee87e0b1bfcf8d24c6c4225d3ecd2971
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-02-26 23:45:11 +0700

    style: remove  card status armada & adjust text on alert perawatan

--- resources/views/crew/dashboard.blade.php
+++ resources/views/crew/dashboard.blade.php
@@ -6,35 +6,11 @@
   <div class="col-12">
     @if($alertPerawatan)
     <div class="alert alert-warning" role="alert">
-      <span class="fe fe-alert-triangle fe-16 mr-2"></span> Waktunya Melakukan Perawatan Berkala
+      <span class="fe fe-alert-triangle fe-16 mr-2"></span> Waktunya Melakukan Perawatan Berkala, Segera Lakukan
+      Perawatan, Terakhir dilakukan pada {{ $alertPerawatan->tanggal }}
     </div>
     @endif
     <div class="row">
-      <div class="col-md-6 col-xl-3 mb-4">
-        <div class="card shadow bg-primary text-white border-0">
-          <div class="card-body">
-            <div class="row align-items-center">
-              <div class="col-3 text-center">
-                <span class="circle circle-sm bg-primary-light">
-                  <i class="fe fe-16 fe-shopping-bag text-white mb-0"></i>
-                </span>
-              </div>
-              <div class="col pr-0">
-                <p class="small text-muted mb-0">
-                  Status Armada
-                </p>
-                <span class="h3 mb-0 text-white">
-                  @if($statusLayakJalan)
-                  Layak Jalan
-                  @else
-                  Tidak Layak Jalan
-                  @endif
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <div class="col-md-6 col-xl-3 mb-4">
         <div class="card shadow border-0">
           <div class="card-body">
