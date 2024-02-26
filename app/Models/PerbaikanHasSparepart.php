45e260a6a757064acb5e7fb0fb7136aaede9753f
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-02-26 23:38:25 +0700

    feat: adding many to many relation to perbaikan

--- app/Models/PerbaikanHasSparepart.php
+++ app/Models/PerbaikanHasSparepart.php
@@ -9,4 +9,14 @@ class PerbaikanHasSparepart extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function detail()
+    {
+        return $this->belongsTo(Sparepart::class, 'sparepart_id');
+    }
+
+    public function perbaikan()
+    {
+        return $this->belongsTo(Perbaikan::class, 'perbaikan_id');
+    }
 }
