4bc3ea4aeb58215f82b871be8073a57da89214b8
Banisaputra <bani.s26shafa@gmail.com>
2024-03-04 10:58:18 +0700

    fix: delete file upload multiple

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,23 +14,4 @@ class HomeController extends Controller
     {
         return view('kepala_gudang.dashboard');
     }
-
-    public function multiUpload() {
-        
-        return view('welcome');
-    }
-
-    public function storeUpload(Request $request) {
-        
-        // dd($request->file('file_upload'));
-        $fileName = time() . '.' . $request->file('file_upload')->extension();
-        if($request->hasFile('file_upload')) {
-            $request->file('file_upload')->store('uppy', 'public', $fileName);
-        }
-
-        return response()->json(['success' => true], 200);
-
-
-
-    }
 }
