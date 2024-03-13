7a97c9ba5c5eb4b7fd599a066f44f2d4a96b3ddb
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-03-13 11:35:16 +0700

    refactor: error handle when alert perawatan is null

--- resources/views/kepala_gudang/dashboard.blade.php
+++ resources/views/kepala_gudang/dashboard.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.main')
 @include('sidebar.menu_kepala_gudang')
-
 @section('content')
 <div class="row justify-content-center">
   <div class="col-12">
-    @if($alertPerawatan)
+    @if($alertPerawatan != null)
     @foreach($alertPerawatan as $item)
     <div class="alert alert-warning" role="alert">
       <span class="fe fe-alert-triangle fe-16 mr-2"></span>Bus <span style="font-weight: 700;">
@@ -17,6 +16,11 @@
       </span>
     </div>
     @endforeach
+    @else
+    <div class="alert alert-success" role="alert">
+      <span class="fe fe-check-circle fe-16 mr-2"></span>Semua Bus Sedang Dalam
+      Kondisi Baik
+    </div>
     @endif
   </div>
 </div> <!-- .row -->
